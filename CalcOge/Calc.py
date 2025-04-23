b3450895825e0f12989fe40ec4f691ed7062eed8
Scholch <alf.201105@gmail.com>
2025-04-23 22:30:18 +0300

    Update Calc create CalcToPathUrl

--- CalcOge/Calc.py
+++ CalcOge/Calc.py
@@ -8,24 +8,15 @@ def convert_to(number, base):
 
 
 class CalcToNotation:
-    def __init__(self, data, base=10, translation=10, printer=True, many=False):
-        if not data or not isinstance(data, str) and not many:
-            raise Exception(f'Incorrect input. {f'Was used {type(data)}.' if not isinstance(data, str)
+    def __init__(self, data, translation, printer=True):
+        if not data or not isinstance(data, (list, tuple)):
+            raise Exception(f'Incorrect input. {f'Was used {type(data)}.' if not isinstance(data, (list, tuple))
                             else 'Was transferred empty object.'}')
-        elif isinstance(data, str) and not many:
-            data = [(data, base)]
-
-        if not data or not isinstance(data, list) and many:
-            raise Exception(f'Incorrect input. {f'Was used {type(data)}.' if not isinstance(data, list)
-                            else 'Was transferred empty object.'}')
-
-        if base < 2 or base > 36:
-            raise Exception(f'Incorrect input. You entered base "{base}"')
+        elif isinstance(data, tuple):
+            data = list((data,))
         if translation < 2 or translation > 36:
             raise Exception(f'Incorrect input. You entered translate "{translation}"')
-        self.printer = printer
         self.translate = translation
-        self.many = many
 
         self.results = []
         for i in range(len(data)):
@@ -36,15 +27,28 @@ class CalcToNotation:
             else:
                 result = convert_to(int(data[i][0], data[i][1]), self.translate)
             self.results.append(result)
-            if self.printer:
+            if printer:
                 print(f'{data[i][0]} ({data[i][1]}) = {result} ({self.translate})')
 
     def comparison(self):
-        if not self.many:
-            raise Exception(f'Incorrect settings. Impossible to use "comparison" without "many"')
-        print(f'Max: {convert_to(max(int(self.results[i], self.translate) for i in range(len(self.results))), self.translate)} '
-              f'|| Min: {convert_to(min(int(self.results[i], self.translate) for i in range(len(self.results))), self.translate)}')
-        return False
+        maximum = convert_to(max(int(self.results[i], self.translate) for i in range(len(self.results))), self.translate)
+        minimum = convert_to(min(int(self.results[i], self.translate) for i in range(len(self.results))), self.translate)
+        print(f'Max: {maximum} || Min: {minimum}')
+        return maximum, minimum
 
 
-CalcToNotation([('2198129', 10), ('2198rte', 30), ('219812oiu9', 31), ('219812poiop9', 32)], translation=16, many=True).comparison()
+class CalcToPathUrl:
+    def __init__(self, data, protocol, server, file):
+        alfa = 'АБВГДЕЖ'
+        data = data.split(' ')
+        server, SExtension = server.split('.')
+        file, FExtension = file.split('.')
+        result, alfa_result, delta_result = '', '', ''
+        for j in protocol, '://', server, '.' + SExtension, '/', file, '.' + FExtension:
+            for i in range(len(data)):
+                if j.lower() == data[i].lower():
+                    result += str(i + 1)
+                    alfa_result += alfa[i]
+                    delta_result += j
+                    break
+        print(f'Output: "{delta_result}" || Answer "{result}" or "{alfa_result}"')
